607195cd9c0e5d62320e34ea202a6d34c1fd5c39
yimyc <yimyc@YILMAR>
2024-02-03 18:28:29 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMinHub/Models/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMinHub.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "m.ale@gmail.com", FirstName="Alejandro", LastName="Martinez", Password="abc"},
+                    new Client { Email = "mariap@gmail.com", FirstName="Maria", LastName="Pola", Password="cde"},
+                    new Client { Email = "Tati88@gmail.com", FirstName="Tatiana", LastName="Castillo", Password="efg"},
+                    new Client { Email = "Joseb@gmail.com", FirstName="Jose", LastName="Bora", Password="hij"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+        }
+    }
+}
